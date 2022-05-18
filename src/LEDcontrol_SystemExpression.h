ab306a11680c3bfdb6b2c44c93677a4d3deba22d
xydtmx <1092999453@qq.com>
2022-05-18 19:53:20 +0800

    .

---
+++ src/LEDcontrol_SystemExpression.h
@@ -0,0 +1,17 @@
+//
+// Created by tamashi on 2022/5/18.
+//
+
+#ifndef RINACHANBOARD_LEDCONTROL_SYSTEMEXPRESSION_H
+#define RINACHANBOARD_LEDCONTROL_SYSTEMEXPRESSION_H
+
+extern String data;
+extern Adafruit_NeoPixel Ledset1;
+extern Adafruit_NeoPixel Ledset2;
+extern Adafruit_NeoPixel Ledset3;
+extern Adafruit_NeoPixel Ledset4;
+extern Adafruit_NeoPixel Ledset5;
+
+void SystemExpression();
+
+#endif //RINACHANBOARD_LEDCONTROL_SYSTEMEXPRESSION_H
